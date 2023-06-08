826e46c8ce9570301db310f720d8e986ade640e4
Dachrono <bluefire_andy@hotmail.com>
2023-06-08 12:26:42 -0600

    web server live

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
     entry: {
         index: './src/index.js',
         print: './src/print.js',
+      },
+      devtool: 'inline-source-map',
+      devServer: {
+        static: './dist',
       },
       plugins: [
         new HtmlWebpackPlugin({
@@ -15,6 +20,9 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
