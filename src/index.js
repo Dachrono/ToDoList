a7bde685d2f725c1a633106ec51d73eeac8dbeba
Dachrono <bluefire_andy@hotmail.com>
2023-06-08 12:10:39 -0600

    add css and images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import './style.css';
+import Icon from './pics/trash.png'
 
 function component() {
     const element = document.createElement('div');
@@ -7,6 +9,13 @@ function component() {
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+     // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,24 @@ module.exports = {
       },
       plugins: [
         new HtmlWebpackPlugin({
-          title: 'Output Management',
+          template: './src/index.html',
         }),
       ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
