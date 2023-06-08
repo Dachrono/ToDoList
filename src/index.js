7cffab8799fe9a1bc140cb7f7f09253e1cf85e47
Dachrono <bluefire_andy@hotmail.com>
2023-06-08 11:55:46 -0600

    htmlwebpackplugin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+  
+    element.appendChild(btn);
   
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Output Management',
+        }),
+      ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
