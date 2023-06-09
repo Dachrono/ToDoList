5f48f4dbf187381b43ade35df4b0ac804e8d9160
Dachrono <bluefire_andy@hotmail.com>
2023-06-09 10:03:18 -0600

    update import links, parameter loader updated with tasklist array

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import './style.css';
 import rel from './pics/reload.png';
 import ent from './pics/enter.png';
-import loader from './Modules/loader.js';
-import { addtask } from './Modules/add.js';
+import taskList from './Modules/taskList.js';
+import loader from './Modules/taskTemplateLoder.js';
 
 const message = document.querySelector('.message');
 message.style.display = 'none';
@@ -14,12 +14,4 @@ img.src = rel;
 const img2 = document.getElementById('enter');
 img2.src = ent;
 
-loader();
-
-const task = document.getElementById('inputTask');
-task.addEventListener('keydown', (e) => {
-  if (e.keyCode === 13) {
-    addtask();
-    task.value = '';
-  }
-});
+loader(taskList);
