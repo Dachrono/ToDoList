00bd73c9b36b99f1a718b15827aaec6ac4f5e837
Dachrono <bluefire_andy@hotmail.com>
2023-06-09 10:08:09 -0600

    created array with task data

---
+++ src/Modules/taskList.js
@@ -0,0 +1,14 @@
+const taskList = [
+  {
+    description: 'repair my code',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'check de change required',
+    completed: false,
+    index: 1,
+  },
+];
+
+export default taskList;
