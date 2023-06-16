a2dd317a7eb7a43a2d6597d27089ef2c0cd503da
Dachrono <bluefire_andy@hotmail.com>
2023-06-16 09:38:31 -0600

    update task test

--- src/test/addRemove.test.js
+++ src/test/addRemove.test.js
@@ -1,5 +1,6 @@
 import { removeData } from '../Modules/removeTask.js';
 import { addtask } from '../Modules/addTask.js';
+import edit from '../Modules/editDescrip.js';
 
 describe('add new task', () => {
   test('add new list task', () => {
@@ -85,5 +86,37 @@ describe('remove old task', () => {
     removeData(arrtemp, buttons[1], 2);
     const liList = document.querySelectorAll('#listTask li');
     expect(liList).toHaveLength(2);
+  });
+});
+
+describe('update value', () => {
+  test('check update value', () => {
+    document.body.innerHTML = `
+            <div class="message">
+                <p>Congrats!! you do not have task!</p>
+            </div>
+            <input id="inputTask" value="test value" />
+            <ul id="listTask">
+                <li>
+                 <input class="checkbox" type="checkbox" data-id="1">
+                 <p class="pt">hell</p>
+                 <a class="remove" id="1"><img class="trash" alt="trash"></a>
+                </li>
+                <li>
+                 <input class="checkbox" type="checkbox" data-id="2">
+                 <p class="pt">hell lvl 2</p>
+                 <a class="remove" id="2"><img class="trash" alt="trash"></a>
+                </li>
+                <li>
+                 <input class="checkbox" type="checkbox" data-id="3">
+                 <p class="pt">hell lvl 3</p>
+                 <a class="remove" id="3"><img class="trash" alt="trash"></a>
+                </li>
+            </ul>
+        `;
+    const descriptive = document.querySelectorAll('.pt');
+    descriptive[1].textContent = 'someone other value';
+    const anotherhell = edit(descriptive[1].textContent, 1);
+    expect(anotherhell).toMatch('someone other value');
   });
 });
